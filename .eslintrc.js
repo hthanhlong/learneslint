d9fb2efbd3a86df02629ecfd59c228e9a19874bf
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-08-22 13:33:35 +0700

    we have one variable deprecated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 module.exports = {
   root: true,
   env: {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
