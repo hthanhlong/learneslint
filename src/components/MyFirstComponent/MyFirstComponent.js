6be9673b9fcf256e08c108bf83914d92b42ac5ff
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-08-22 14:32:20 +0700

    update

---
+++ src/components/MyFirstComponent/MyFirstComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './style.scss'
+const MyFirstComponent = () => {
+  return <div>hello MyFirstComponent</div>
+}
+
+export default MyFirstComponent
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+// export { MyFirstComponent as default } from './MyFirstComponent'
