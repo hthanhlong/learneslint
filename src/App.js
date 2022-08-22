95fa067f94ab6e34245cfb27a17c9bc3ee8292e1
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-08-22 14:03:34 +0700

    test eslint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from 'react'
+import MyFirstComponent from './components/MyFirstComponent'
 const App = () => {
-  console.warn('hello, please remove me');
-  console.error('hello, please remove me');
-  return <h1>Hello React</h1>;
-};
+  console.warn('hello, please remove me')
+  console.error('hello, please remove me')
+  return (
+    <h1>
+      Hello React 2 <MyFirstComponent />
+    </h1>
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/MyFirstComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MyFirstComponent = () => {
+  return <div>hello MyFirstComponent</div>
+}
+
+export default MyFirstComponent
