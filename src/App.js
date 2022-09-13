9569e1e33a8656b5dbbb2bd1eed43488190ce7e6
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-09-13 23:12:47 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import { Slide } from '@'
-import { Fragment, useReducer } from 'react'
+import { Fragment, useReducer, useEffect } from 'react'
 import { slidesData } from './mockdata'
 import { initialState, slidesReducer } from './reducer'
 
 const App = () => {
   const [state, dispatch] = useReducer(slidesReducer, initialState)
 
+  useEffect(() => {
+    if (intervalID) clearInterval(intervalID)
+    let intervalID = setInterval(() => {
+      dispatch({ type: 'NEXT' })
+    }, 2000)
+    return () => {
+      if (intervalID) clearInterval(intervalID)
+    }
+  }, [])
+
   return (
     <Fragment>
       <div className="slides">
-        <button onClick={() => dispatch({ type: 'PREV' })}>‹</button>
         {[...slidesData, ...slidesData, ...slidesData].map((slide, i) => {
           let offset = slidesData.length + (state.slideIndex - i)
           return <Slide slide={slide} offset={offset} key={i} />
         })}
-        <button onClick={() => dispatch({ type: 'NEXT' })}>›</button>
       </div>
-      );
     </Fragment>
   )
 }
--- src/mockdata.js
+++ src/mockdata.js
@@ -2,23 +2,18 @@ import { image1, image2, image3, image4, image5 } from './assets/imgs'
 
 export const slidesData = [
   {
-    title: 'Thanh Long Thuy My',
     image: image1,
   },
   {
-    title: 'Thanh Long Thuy My',
     image: image2,
   },
   {
-    title: 'Thanh Long Thuy My',
     image: image3,
   },
   {
-    title: 'Thanh Long Thuy My',
     image: image4,
   },
   {
-    title: 'Thanh Long Thuy My',
     image: image5,
   },
 ]
